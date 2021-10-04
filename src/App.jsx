88ffeac512828a02520493e5f32ba09aeeec4c3d
babaiyu <bayuputra9a@gmail.com>
2021-10-04 10:50:47 +0000

    Integrated with Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./components/Home";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ChakraProvider>
+      <Home />
+    </ChakraProvider>
+  );
 }
