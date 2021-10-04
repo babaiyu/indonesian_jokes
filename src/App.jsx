5442e347aaf7162e40346ed90a7712025c493fcb
babaiyu <bayuputra9a@gmail.com>
2021-10-04 11:18:20 +0000

    Integrate with new theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./components/Home";
+import theme from "./theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Home />
     </ChakraProvider>
   );
