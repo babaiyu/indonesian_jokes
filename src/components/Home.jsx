63a032518ba79d151b7dce9b37df19bf125aca06
babaiyu <bayuputra9a@gmail.com>
2021-10-04 10:50:29 +0000

    Created Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import { Container } from "@chakra-ui/react";
+import Header from "./Header";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
