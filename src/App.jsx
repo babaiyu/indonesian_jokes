6d6908735f6122a81823c1f83bdad3a5bbd900d8
babaiyu <bayuputra9a@gmail.com>
2021-10-06 21:19:35 +0000

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import theme from "./theme";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Header />
       <Home />
+      <Footer />
     </ChakraProvider>
   );
 }
