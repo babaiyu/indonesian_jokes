ee4ee3f0971aa3ebaff8fe544e65198157ecc513
babaiyu <bayuputra9a@gmail.com>
2021-10-06 21:19:24 +0000

    Update home with using example List

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import { Container, Divider } from "@chakra-ui/react";
-import Header from "./Header";
+import List from "./List";
+
+const data = [1, 2, 3, 4, 5, 6];
 
 export default function Home() {
   return (
-    <Container>
-      <Header />
-      <Divider orientation="horizontal" colorScheme="blue" />
+    <Container maxW="container.lg" centerContent>
+      <Divider orientation="horizontal" colorScheme="blue" marginBottom="10" />
+      <List data={data} />
     </Container>
   );
 }
