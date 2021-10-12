d817af51f3578a1eaa9ea12770bb04f9624f4633
babaiyu <bayuputra9a@gmail.com>
2021-10-12 16:00:58 +0700

    Update generate.js (Get github profile by using author from json file

--- generate.js
+++ generate.js
@@ -1,23 +1,48 @@
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios").default;
 
+const baseUrl = "https://api.github.com/users";
 let arr = [];
 
+// Get github profile by username
+async function getUser(username, data) {
+  try {
+    const response = await axios.get(`${baseUrl}/${username}`);
+    const resData = response.data || { avatar: "", name: "" };
+
+    data.map((item) => {
+      const payload = {
+        id: +new Date(),
+        jokes: item.jokes,
+        avatar: resData.avatar_url,
+        name: resData.name,
+      };
+
+      arr.push(payload);
+    });
+
+    fs.writeFileSync(
+      "./src/assets/jokes.json",
+      JSON.stringify({ data: arr }, null, 4)
+    );
+
+    console.table(arr);
+    console.log("Generate jokes is complete!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Get json file
 const jsonsInDir = fs
   .readdirSync("./src/data")
   .filter((file) => path.extname(file) === ".json");
 
-jsonsInDir.forEach(async (file) => {
+// Read each of item from src/data
+jsonsInDir.forEach((file) => {
   const fileData = fs.readFileSync(path.join("./src/data", file));
   const jsonParsed = JSON.parse(fileData.toString());
 
-  arr = [...arr, ...jsonParsed.data];
-
-  fs.writeFileSync(
-    "./src/assets/jokes.json",
-    JSON.stringify({ data: arr }, null, 4)
-  );
+  getUser(jsonParsed.author, jsonParsed.data);
 });
-
-console.table(arr);
-console.log("Generate jokes is complete!");
