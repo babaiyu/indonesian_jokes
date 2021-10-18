2f9032e32b499084121b321a9968a0930e306c7d
Albert Felix <albertfelixleo@gmail.com>
2021-10-18 11:31:50 +0530

    created toggle button for light-dark theme #17

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,10 +1,35 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  fonts: {
-    heading: "Open Sans",
-    body: "Open Sans",
-  },
-});
 
-export default theme;
+export const lightTheme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "white",
+      },
+      fonts: {
+        heading: "Open Sans",
+        body: "Open Sans",
+      },
+    },
+  }
+})
+
+export const darkTheme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "black",
+        color:"#EDF3CB"
+      },
+      fonts: {
+        heading: "Open Sans",
+        body: "Open Sans",
+      },
+    },
+  }
+})
+
+export default lightTheme
