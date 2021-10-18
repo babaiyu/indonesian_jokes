c4f1ee7f06ffe4c78db2d54e21933aa844b7f250
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-10-18 16:38:19 +0530

    Completed: Create "forked me" on header #22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,38 @@
-import { Box, Center, Heading, Text, Button } from "@chakra-ui/react";
+import {Box, Center, Heading, Text, Button} from "@chakra-ui/react";
 
 export default function Header(props) {
-  
-  return (
-    <Box marginTop="20" marginBottom="10">
-      <Center marginBottom="5">
-        <Heading
-          as="h2"
-          size="2xl"
-          bgGradient="linear(to-l, #7928CA, #FF0080)"
-          bgClip="unset"
-          fontWeight="extrabold"
-        >
-          Indonesia Jokes
-        </Heading>
-        <Box 
-          marginLeft="20"
-          border="1px" borderColor="grey"
-          bg={props.isLightMode ? "black" : "white"} 
-          borderRadius="10" w="3rem" h="5" 
-          onClick = {props.toggleTheme}>
-            <Box 
-              float={props.isLightMode ? "left" : "right"} 
-              bg={props.isLightMode ? "white" : "black"} 
-              borderRadius="10" w="5" h="5">
-            </Box>
+
+    return (
+        <Box marginTop="20" marginBottom="10">
+            <Center marginBottom="5">
+                <a className="message" href="https://github.com/babaiyu/indonesian_jokes">forked me </a>
+                <Heading
+                    as="h2"
+                    size="2xl"
+                    bgGradient="linear(to-l, #7928CA, #FF0080)"
+                    bgClip="unset"
+                    fontWeight="extrabold"
+                >Indonesia Jokes
+                </Heading>
+                <Box
+                    marginLeft="20"
+                    border="1px" borderColor="grey"
+                    bg={props.isLightMode ? "black" : "white"}
+                    borderRadius="10" w="3rem" h="5"
+                    onClick={props.toggleTheme}>
+                    <Box
+                        float={props.isLightMode ? "left" : "right"}
+                        bg={props.isLightMode ? "white" : "black"}
+                        borderRadius="10" w="5" h="5">
+                    </Box>
+                </Box>
+            </Center>
+            <Center>
+                <Text textAlign="center" color="gray.600">
+                    Kumpulan jokes lucu yang tersedia dalam bahasa Indonesia. Project ini
+                    dibuat untuk memeriahkan #hacktoberfest2021
+                </Text>
+            </Center>
         </Box>
-      </Center>
-      <Center>
-        <Text textAlign="center" color="gray.600">
-          Kumpulan jokes lucu yang tersedia dalam bahasa Indonesia. Project ini
-          dibuat untuk memeriahkan #hacktoberfest2021
-        </Text>
-      </Center>
-    </Box>
-  );
+    );
 }
