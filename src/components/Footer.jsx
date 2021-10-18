329b09aa3c47c049ba1e9daa68d14ca7d5f7c905
Pururaj Singh Rajawat <pururajsrajawwat@gmail.com>
2021-10-18 12:14:35 +0530

    Made the year in footer dynamic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Stack, Divider, Center, Text } from "@chakra-ui/react";
 
+const year = new Date().getFullYear();
+
 export default function Footer() {
   return (
     <Stack marginTop="20" marginBottom="10">
       <Divider />
       <Center>
-        <Text fontSize="sm">Copyright &copy; 2021</Text>
+        <Text fontSize="sm">Copyright &copy; {year}</Text>
       </Center>
     </Stack>
   );
