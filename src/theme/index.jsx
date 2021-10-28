f78bf6e72c56ce060a217962e5bd647f793cf288
bayupergradana <bayu@gradana.com>
2021-10-28 09:15:32 +0700

    Change color

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,35 +1,34 @@
 import { extendTheme } from "@chakra-ui/react";
 
-
 export const lightTheme = extendTheme({
   styles: {
     global: {
       // styles for the `body`
       body: {
-        bg: "white",
+        bg: "#F0F0F0",
       },
       fonts: {
         heading: "Open Sans",
         body: "Open Sans",
       },
     },
-  }
-})
+  },
+});
 
 export const darkTheme = extendTheme({
   styles: {
     global: {
       // styles for the `body`
       body: {
-        bg: "black",
-        color:"#EDF3CB"
+        bg: "#222222",
+        color: "#F0F0F0",
       },
       fonts: {
         heading: "Open Sans",
         body: "Open Sans",
       },
     },
-  }
-})
+  },
+});
 
-export default lightTheme
+export default lightTheme;
