cce1709f9bd0ae85ce18e9a361680a572070f7e9
bayupergradana <bayu@gradana.com>
2021-10-28 09:28:26 +0700

    Create helpers LazyLoad

---
+++ src/helpers/LazyLoad.jsx
@@ -0,0 +1,12 @@
+import { Suspense } from "react";
+import { Button, Center } from "@chakra-ui/react";
+
+export default function LazyLoad({ children }) {
+  return <Suspense fallback={<ButtonLoading />}>{children}</Suspense>;
+}
+
+const ButtonLoading = () => (
+  <Center>
+    <Button isLoading colorScheme="blue" variant="solid" />˝
+  </Center>
+);
