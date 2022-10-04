a451426140d68a88d5a94269e109456b47f50449
Naufal Fawwaz <fawaznaufal23@gmail.com>
2022-10-04 21:51:44 +0700

    Add jokes and improve styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import {Box, Center, Heading, Text, Tooltip, Image} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import {Box, Center, Heading, Text, Tooltip, Image, HStack} from "@chakra-ui/react";
 import darkicon from "../assets/darkicon.png"
 
 export default function Header(props) {
@@ -6,31 +7,40 @@ export default function Header(props) {
     return (
         <Box marginTop="20" marginBottom="10">
             <Center marginBottom="5">
-                <a href="https://github.com/babaiyu/indonesian_jokes" target="_blank">
-                    <Tooltip hasArrow label="Fork me!" bg="gray.300" color="black" placement="right-start">
-                        <img className="iconStyle" src={darkicon} alt="github-logo"/>
-                    </Tooltip>
-                </a>
+                <HStack className={"top-right-links"}>
+                    <Box
+                        marginRight="4"
+                        padding="1.5"
+                        border="1px" borderColor="grey"
+                        bg={props.isLightMode ? "white" : "black"}
+                        borderRadius='2xl'
+                        w={'16'}
+                        _hover={{ 
+                            'cursor': 'pointer'
+                        }}
+                        onClick={props.toggleTheme}>
+                        {props.isLightMode ? (
+                            <SunIcon w={4} h={4} float={'right'} />
+                        ) : (
+                            <MoonIcon w={4} h={4} float={'left'} />
+                        )}
+                    </Box>
+                    <a href="https://github.com/babaiyu/indonesian_jokes" target="_blank">
+                        <Tooltip hasArrow label="Fork me!" bg="gray.300" color="black" placement="right-start">
+                            <img className="iconStyle" src={darkicon} alt="github-logo"/>
+                        </Tooltip>
+                    </a>
+                </HStack>
                 <Heading
                     as="h2"
                     size="2xl"
                     bgGradient="linear(to-l, #7928CA, #FF0080)"
                     bgClip="unset"
                     fontWeight="extrabold"
+                    padding={2}
+                    rounded={8}
                 >Indonesia Jokes
                 </Heading>
-                <Box
-                    marginLeft="20"
-                    border="1px" borderColor="grey"
-                    bg={props.isLightMode ? "black" : "white"}
-                    borderRadius="10" w="3rem" h="5"
-                    onClick={props.toggleTheme}>
-                    <Box
-                        float={props.isLightMode ? "left" : "right"}
-                        bg={props.isLightMode ? "white" : "black"}
-                        borderRadius="10" w="5" h="5">
-                    </Box>
-                </Box>
             </Center>
             <Center>
                 <Text textAlign="center">
