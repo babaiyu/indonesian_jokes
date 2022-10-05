a02284d2a8afdd1a58294a5698465472a7b7e2ab
Dody <dodyags@gmail.com>
2022-10-06 05:14:54 +0700

    fixed Divider not visible in Light theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import { Stack, Divider, Center, Text } from "@chakra-ui/react";
 
 const year = new Date().getFullYear();
 
-export default function Footer() {
+export default function Footer(props) {
   return (
     <Stack marginTop="20" marginBottom="10">
-      <Divider />
+      <Divider borderColor={props.isLightMode ? "black" : "white"} />
       <Center>
         <Text fontSize="sm">Copyright &copy; {year}</Text>
       </Center>
